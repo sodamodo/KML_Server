f3811189bc151494a23b63b7fe96fd6aa2f538c7
Def <lamphead@gmail.com>
2015-07-29 15:42:07 -0400

    I realized I need to download the original KML from the web. I think I can do this actually. Im doing a push now because Im about to do delete the extranious files

--- NetworkLink/views.py
+++ NetworkLink/views.py
@@ -1,112 +1,3 @@
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# import os
-# from django.conf import settings
-# from django.core.servers.basehttp import FileWrapper
-# from KML_Server.settings import BASE_DIR
-# from bs4 import BeautifulSoup
-# import requests
-# import csv
-# from collections import defaultdict
-# from django.http import HttpResponse
-#
-#
-# # Create your views here.
-#
-# def getpoints(request):
-#     r = requests.get('http://waterservices.usgs.gov/nwis/iv/?format=waterml,1.1&indent=on&stateCd=ga&parameterCd=00060,00065&siteType=ST')
-#
-#     soup = BeautifulSoup(r.text, "lxml-xml")
-#
-#
-#     names = []
-#     for r in soup.find_all("siteName"):
-#         names.append(r.contents)
-#
-#
-#     latitude = []
-#     for lat in soup.find_all("latitude"):
-#         latitude.append(lat.contents)
-#
-#     longitude = []
-#     for lon in soup.find_all("longitude"):
-#         longitude.append(lon.contents)
-#
-#
-#     values = []
-#     for v in soup.find_all("value"):
-#         values.append(v.contents)
-#
-#     nameform = []
-#     for name in names:
-#         nameform.append(name[0].encode("utf-8"))
-#
-#     latform = []
-#     for lat in latitude:
-#         latform.append(float(lat[0]))
-#
-#     lonform = []
-#     for lon in longitude:
-#         lonform.append(float(lon[0]))
-#
-#
-#     valform = []
-#     for val in values:
-#         valform.append(float(val[0]))
-#
-#
-#
-#     f = open('raw_points.csv', 'wb')
-#
-#     # writer = csv.DictWriter(open('points.csv', 'w'), delimiter=',', lineterminator='\n', fieldnames=['name', 'latitude', 'longitude', 'value'], extrasaction='ignore')
-#     # writer = csv.writer('points.csv', 'wb')
-#     writer = csv.writer(f)
-#     # writer.writerow(('name', 'latitude', 'longitude', 'value'))
-#
-#     aggregated = []
-#     for i in xrange(0, len(nameform)):
-#         aggregated.append([names[i][0], float(latitude[i][0]), float(longitude[i][0]), float(values[i][0])])
-#         print aggregated[i]
-#         # writer.writerow(aggregated[i])
-#
-#     data_dict = defaultdict(list)
-#     for i in xrange(0, len(aggregated)):
-#         for k, v in (('name', aggregated[i][0]), ('latitude', aggregated[i][1]), ('longitude', aggregated[i][2]), ('value', aggregated[i][3])):
-#             data_dict[k].append(v)
-#
-#
-#     # print data_dict.get('latitude')
-#
-#     name = data_dict.get('name')
-#     latitude = data_dict.get('latitude')
-#     longitude = data_dict.get('longitude')
-#     value = data_dict.get('value')
-#     # print value
-#
-#     raw_data = []
-#     for i in xrange(0, len(aggregated)):
-#         # raw_data.append("{}, {}, {}, {}".format([name[i], latitude[i], longitude[i], value[i]]))
-#         writer.writerow([name[i], latitude[i], longitude[i], value[i]])
-#     # raw_data.append([name[i], latitude[i], longitude[i], value[i]])
-#     # writer.writerow("{}, {}, {}, {}".format(name[i], latitude[i], longitude[i], value[i]))
-#     print f
-#     # print(data_dict)
-#
-#     f.close()
-#     return HttpResponse('')
-#
-#
-# def path(request):
-#     load_path = os.path.join(settings.BASE_DIR, "NetworkLink", "static", "points.kml")
-#
-#     downloadfile = open(load_path, 'rb')
-#
-#     response = HttpResponse(FileWrapper(downloadfile), content_type='application/vnd.google-earth.kml+xml')
-#     response['Content-Disposition'] = 'attachment; filename=loader.kml'  # make custom download name
-#     return response
-
-
-
 import xml.etree.ElementTree as etree
 from lxml.etree import ElementTree, Element, SubElement, tostring
 from xml.dom import minidom
@@ -120,6 +11,7 @@ from KML_Server import settings
 import os
 from django.core.servers.basehttp import FileWrapper
 from django.http import HttpResponse
+import NetworkLink
 
 
 # KML_HOLDER = os.path.join(BASE_DIR)
@@ -132,19 +24,12 @@ i = 0
 def placemark(row):
 
     placemark = Element('Placemark', targetId=str(i))
-    # treeElement = ElementTree(placemark)
     name = Element('name')
 
 
     extended_data = Element('ExtendedData')
-    # data = Element('Data', name="holeNumber")
-
-    # displayname = Element('displayName')
-    # displayname.text = "Hiiiii!"
     name = Element('name')
     name.text = row[0]
-
-
 
     streamtitle = Element('Data', name="Rivername")
     streamflow = Element('Data', name="Stream Flow")
@@ -233,7 +118,6 @@ def makelists():
 
 
 def fire(request):
-
     kml = Element('kml', xmlns="http://www.opengis.net/kml/2.2")
     document = Element("Document")
     document.text = "doooz" # delete later
@@ -255,25 +139,25 @@ def fire(request):
     folder = Element("Folder")
 
     for row in rows:
-        # print row.name, row.lat, row.long, row.data
         document.append(placemark(row))
-        # print(tostring((document)))
-
 
     print tostring(kml, pretty_print=True)
 
 
-    # f.write(tostring(os.path.join(BASE_DIR, 'KML', kml)))
+    f.write(tostring(os.path.join('KML_Server', 'static')))
     f.write(tostring(kml))
     f.close()
-    print "done!"
+
+
+    return HttpResponse('')
+    # print "done!"
+    # # #
+    # downloadfile = open('teh_kml.kml', 'rb')
     #
-    downloadfile = open('teh_kml.kml', 'rb')
-
-
-    response = HttpResponse(FileWrapper(downloadfile), content_type='application/vnd.google-earth.kml+xml;')
-    response['Content-Disposition'] = 'attachment; kml.kml'  # make custom download name
-    return response
+    #
+    # response = HttpResponse(FileWrapper(downloadfile), content_type='application/vnd.google-earth.kml+xml;')
+    # response['Content-Disposition'] = 'attachment; kml.kml'  # make custom download name
+    # return response
             # return HttpResponseRedirect(reverse('fly.views.upload'))
 
 
