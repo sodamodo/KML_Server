1f83bee476951fc3b801c0c3c18dcebbd513b434
Zack <lamphead@gmail.com>
2016-05-28 08:28:40 -0400

    Fixed doubling problem by skipping every other row when calling the placemark function.  Removed custom styling.

--- NetworkLink/views.py
+++ NetworkLink/views.py
@@ -10,10 +10,12 @@ from collections import defaultdict, namedtuple
 from django.views.static import serve
 from KML_Server import settings
 import os
-from django.core.servers.basehttp import FileWrapper
+from wsgiref.util import FileWrapper
 from django.http import HttpResponse
 import NetworkLink
 from django.shortcuts import render
+from django.utils.encoding import smart_str
+import sys
 
 # KML_HOLDER = os.path.join(BASE_DIR)
 
@@ -33,7 +35,7 @@ def placemark(row):
     streamtitle = Element('Data', name="Rivername")
     streamflow = Element('Data', name="Stream Flow")
     streamtitle.text = "{}".format(row.name)
-    streamflow.text = "{}".format(str(row.data))
+    streamflow.text = "{}".format(str(row.data[0]))
     extended_data.append(streamtitle)
     extended_data.append(streamflow)
 
@@ -52,20 +54,20 @@ def placemark(row):
 
 
     ##Styling information
-    style = Element("Style", id="ID")
-    iconstyle = Element('IconStyle', id="ID")
-    scale = Element('scale')
-    iconstyle.append(scale)
-    style.append(iconstyle)
-    placemark.append(style)
-    icon = Element("Icon")
-    iconstyle.append(icon)
-    href = Element("href")
-    href.text = "http://maps.google.com/mapfiles/kml/paddle/purple-circle.png"
-    icon.append(href)
-    color = Element('color')
-    iconstyle.append(color)
-    color.text = '50DC783C'
+    # style = Element("Style", id="ID")
+    # iconstyle = Element('IconStyle', id="ID")
+    # scale = Element('scale')
+    # iconstyle.append(scale)
+    # style.append(iconstyle)
+    # placemark.append(style)
+    # icon = Element("Icon")
+    # iconstyle.append(icon)
+    # href = Element("href")
+    # href.text = "http://maps.google.com/mapfiles/kml/paddle/purple-circle.png"
+    # icon.append(href)
+    # color = Element('color')
+    # iconstyle.append(color)
+    # color.text = '50DC783C'
 
     return placemark
 
@@ -123,51 +125,26 @@ def makelists():
 
 def fire(request):
 
-
-
     kml = Element('kml', xmlns="http://www.opengis.net/kml/2.2")
     document = Element("Document")
-    document.text = "doooz" # d'elete later
     kml.append(document)
-    rows = makelists()
+    points = makelists()
 
-
-
-    # print("ROWS")
     name = Element("name")
     name.text = "Guaging Stations"
     document.append(name)
     # folder = Element("Folder")
 
-    for row in rows:
-        document.append(placemark(row))
-        continue
+    for i in range(0, len(points), 2):
+        document.append(placemark(points[i]))
 
-    placemark_list = []
-    # for row in rows:
-    #     # placemark_list.append()
-    #     # document.append(placemark(row))
-    #     placemark(row)
+
 
     print tostring(kml, pretty_print=True)
 
 
-    # f = open(os.path.join(BASE_DIR, 'NetworkLink', 'static', 'NetworkLink', 'guages.kml'), 'wb')
-    # f.write(tostring(kml))
-    # f.write(tostring(os.path.join('KML_Server', 'static')))
-    # f.close()
-
 
     return HttpResponse(tostring(kml))
-    # print "done!"
-    # # #
-    # downloadfile = open('gauges.kml', 'rb')
-    #
-    #
-    # response = HttpResponse(FileWrapper(downloadfile), content_type='application/vnd.google-earth.kml+mlml;')
-    # response['Content-Disposition'] = 'attachment; kml.kml'  # make custom download name
-    # return response
-            # return HttpResponseRedirect(reverse('fly.views.upload'))
 
 def welcome(request):
     return render(request, 'Welcome.html')
